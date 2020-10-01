dae9275aef49f69c5bb57297b1c18c0510035b51
David Plass <dplass@gmail.com>
2020-10-01 13:22:25 -0400

    Reverted logic analyzer to be a more generic logic analyzer.

--- logic_analyzer/logic_analyzer.ino
+++ logic_analyzer/logic_analyzer.ino
@@ -1,66 +1,82 @@
 /*
-   Simple logic analyzer. Triggers when the data pin goes low.
-   Records bits until 30  1's in a row are received.
+ * Simple logic analyzer. Triggers when the data pin goes low.
+ * Records bits until 30  1's in a row are received.
+ * Attemps to debounce the button (poorly). Resets the board
+ * (reset pin low) when the button is pressed, and starts listening.
 */
 
+const int buttonPin = 7;     // the number of the pushbutton pin
 const int dataPin = 2;      // corresponds to pin 6 on the KB connector (and pin 6 of the RJ12)
 const int resetPin = 5;     // corresponds to pin 2 on the KB connector (and pin 4 on the RJ12)
 
+int buttonState = 0;         // variable for reading the pushbutton status
+
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(buttonPin, INPUT);
   pinMode(resetPin, OUTPUT);
   pinMode(dataPin, INPUT);
 
   // Set the reset pin low for 10 ms.
-  digitalWrite(resetPin , LOW);
+  digitalWrite(resetPin, LOW);
   delay(10);
-  digitalWrite(resetPin , HIGH);
+  digitalWrite(resetPin, HIGH);
 
-  Serial.begin(1000000);
+  Serial.begin(9600);
   Serial.println("Hello logic_analyzer");
 }
 
-const int baudDelay=3333;
+int changed = 0;
+int shouldRead = 0;
 int data = 0;
+int inKey = 0;  // are we reading a key?
+int numOnes = 0;  // the # of 1s in a row
 
 void loop() {
-  data = digitalRead(dataPin);
-  if (data == LOW) {
-    int numOnes = 0;
+  // read the state of the pushbutton value:
+  buttonState = digitalRead(buttonPin);
+
+  // check if the pushbutton is pressed. If it is, the buttonState is HIGH:
+  if (buttonState == HIGH && !changed) {
+    // turn LED on, and toggle recording.
     digitalWrite(LED_BUILTIN, HIGH);
-//    Serial.print("START: ");
-  //  unsigned long started = micros();
-    // Serial.print(0); Serial.print(", 0,  ");
-    int index = 0;
-    int allData[9];
-    int theKey=0;
-    delayMicroseconds(baudDelay/2);
-    while (index <= 8) {
-     // unsigned long now = micros();
-      data = digitalRead(dataPin);
-      //      Serial.print(now - started);
-      allData[index++] = data;
-      if (data == HIGH) {
-        theKey = (theKey>>1)|0x80;
-        //Serial.print(", 1,  ");
-        //Serial.print(1);
-        numOnes++;
-      } else {
-        theKey = (theKey>>1);
-        numOnes = 0;
-        //        Serial.print(", 0,  ");
-        //Serial.print(0);
-      }
-      delayMicroseconds(baudDelay);
-    }
-    Serial.print("TYPED: ");
-    Serial.print((char)theKey);
-    Serial.print(" (");
-    for (int i = 0; i < index; ++i) {
-      Serial.print(allData[i]); 
-    }
-    Serial.println(")");
+    Serial.println("on");
+    shouldRead = 1 - shouldRead;
+    Serial.print("ShouldRead is "); Serial.println(shouldRead);
+    changed = 1;
+  } else if (buttonState == LOW && changed) {
+    // turn LED off:
     digitalWrite(LED_BUILTIN, LOW);
-//    Serial.println("END");
+    Serial.println("off");
+    changed = 0;
+ 
+    // beep/reset, and keep reading.
+    digitalWrite(resetPin , LOW);
+    delay(10);
+    digitalWrite(resetPin , HIGH);
+  }
+
+  if (shouldRead == 1) {
+    data = digitalRead(dataPin);
+    if (data == LOW && !inKey) {
+      inKey=1;  // we're recording until numones==30
+      numOnes=0;
+      Serial.println("START");
+      Serial.println("0");
+    } else if (inKey) {
+      if (data == HIGH) {
+        Serial.println("1");
+        numOnes++;
+        if (numOnes == 30) {
+          inKey=0;
+          numOnes=0;
+          Serial.println("END");
+          delay(500);
+        }
+      } else {
+        numOnes=0;
+        Serial.println("0");
+      }
+    }
   }
 }
