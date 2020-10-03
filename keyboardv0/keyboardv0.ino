c86b3f9d4ee8ba140331b7b65681d804ef6da6a4
David Plass <dplassgit@users.noreply.github.com>
2020-10-03 19:38:17 -0400

    Test sending a 1.3-ish usecond burst

--- keyboardv0/keyboardv0.ino
+++ keyboardv0/keyboardv0.ino
@@ -5,22 +5,26 @@
 
 const int dataPin = 2;      // pin 6 on the KB connector (and pin 6 of the RJ12)
 const int resetPin = 5;     // pin 2 on the KB connector (and pin 4 on the RJ12)
+const int fakeColumn = 8;
 
 void setup() {
   pinMode(resetPin, OUTPUT);
   pinMode(dataPin, INPUT);
+  pinMode(fakeColumn, OUTPUT);
+  DDRB |= B00000001;// make pin 8 an output.
 
+  Serial.begin(9600);
+  Serial.println("Hello keyboard v0");
+
+  digitalWrite(fakeColumn, LOW);
   // Set the reset pin low for 10 ms.
   digitalWrite(resetPin , LOW);
   delay(10);
   digitalWrite(resetPin , HIGH);
-
-  Serial.begin(9600);
-  Serial.println("Hello keyboardv0");
 }
 
 // Microseconds to wait between bits. Corresponds to 110 baud, empirically determined.
-const int baudDelay = 3387;
+const int baudDelay = 3333;
 int allData[9];
 
 void loop() {
@@ -29,6 +33,14 @@ void loop() {
     int index = 0;
     int theKey = 0;
 
+    PORTB = B00000001;
+    //digitalWrite(fakeColumn, HIGH);
+    // one nop = 62.5ns. need it to be 1.4 microseconds = 22ish
+    __asm__("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+    // delayMicroseconds(2);
+    //digitalWrite(fakeColumn, LOW);
+    PORTB = B00000000;
+ 
     // Wait half a cycle so that we're sampling in the middle of the bit.
     delayMicroseconds(baudDelay / 2);
     while (index <= 8) {
