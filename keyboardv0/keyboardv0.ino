2e46bab2d1574206bfc9006b7e028bd18359f917
David Plass <dplassgit@users.noreply.github.com>
2020-10-04 11:48:47 -0400

    Send a 1.4us signal on pin 8 when ABCD match 0010
    IT'S ALIVE! This code restores pin 16 (Y4) on the X22-950

--- keyboardv0/keyboardv0.ino
+++ keyboardv0/keyboardv0.ino
@@ -3,17 +3,27 @@
    Records 8 bits at 110 baud.
 */
 
-const int dataPin = 2;      // pin 6 on the KB connector (and pin 6 of the RJ12)
-const int resetPin = 5;     // pin 2 on the KB connector (and pin 4 on the RJ12)
+const int dataPin = 7;      // pin 6 on the KB connector (and pin 6 of the RJ12)
+const int resetPin = 9;     // pin 2 on the KB connector (and pin 4 on the RJ12)
+const int aPin = 4;
+const int bPin = 5;
+const int cPin = 6;
+const int dPin = 2;
 const int fakeColumn = 8;
+const int outputEnabledPin = 3;
 
 void setup() {
   pinMode(resetPin, OUTPUT);
   pinMode(dataPin, INPUT);
+  pinMode(outputEnabledPin, INPUT);
   pinMode(fakeColumn, OUTPUT);
+  pinMode(aPin, INPUT);
+  pinMode(bPin, INPUT);
+  pinMode(cPin, INPUT);
+  pinMode(dPin, INPUT);
   DDRB |= B00000001;// make pin 8 an output.
 
-  Serial.begin(9600);
+  Serial.begin(1000000);
   Serial.println("Hello keyboard v0");
 
   digitalWrite(fakeColumn, LOW);
@@ -21,11 +31,37 @@ void setup() {
   digitalWrite(resetPin , LOW);
   delay(10);
   digitalWrite(resetPin , HIGH);
+
+  attachInterrupt(digitalPinToInterrupt(outputEnabledPin), intHandler, RISING);
 }
 
 // Microseconds to wait between bits. Corresponds to 110 baud, empirically determined.
 const int baudDelay = 3333;
 int allData[9];
+
+int printed = 0;
+// A B C D
+// 0 0 1 0 = A16 (Y4)
+// 1 0 1 0 = A17 (Y5)
+void intHandler() {
+  // read pins a,b,c,d at the same time.
+  int allPins = PIND & B01110100;
+
+  if (allPins == B01000000) {
+    // pin 16 (Y4) = 0010
+    PORTB = B00000001;
+    // one nop = 62.5ns. need it to be 1.4 microseconds = 22ish
+    __asm__("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+    PORTB = B00000000;
+  }
+  //  if (allPins == B00000000) {
+  //    // pin 12 (Y0) = 0000
+  //    PORTB = B00000001;
+  //    // one nop = 62.5ns. need it to be 1.4 microseconds = 22ish
+  //    __asm__("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
+  //    PORTB = B00000000;
+  //  }
+}
 
 void loop() {
   int data = digitalRead(dataPin);
@@ -33,17 +69,10 @@ void loop() {
     int index = 0;
     int theKey = 0;
 
-    PORTB = B00000001;
-    //digitalWrite(fakeColumn, HIGH);
-    // one nop = 62.5ns. need it to be 1.4 microseconds = 22ish
-    __asm__("nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t");
-    // delayMicroseconds(2);
-    //digitalWrite(fakeColumn, LOW);
-    PORTB = B00000000;
- 
     // Wait half a cycle so that we're sampling in the middle of the bit.
     delayMicroseconds(baudDelay / 2);
     while (index <= 8) {
+
       data = digitalRead(dataPin);
 
       allData[index++] = data;
