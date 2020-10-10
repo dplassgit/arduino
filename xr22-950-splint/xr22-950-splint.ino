9f869d825ca5db44ea94ba06b9c927f3365b9a1d
David Plass <dplassgit@users.noreply.github.com>
2020-10-10 18:05:58 -0400

    Finished defining input pins and output pins. Changed to bytes from int for the splint values.

--- xr22-950-splint/xr22-950-splint.ino
+++ xr22-950-splint/xr22-950-splint.ino
@@ -3,75 +3,71 @@
 
    It listens to the 4 control pins (ABCD) and sends a pulse on the corresponding output pin
    for the broken pins on my XR22-950-3A.
+
+   It's called "splint" because it only replicates part of the XR22-950 functionality.
 */
 
 // This MUST be an interrupt-enabled pin.
 const int outputEnabledPin = 2;
 
-// NOTE: pin 3 is NC
+// Control (input) pins: PF4 (A) through PF7 (D), a.k.a A0 through A3
+// These aren't used because we need to use direct port manipulation to 
+// read all 4 pins at the same time.
+// const int aPin = A3;
+// const int bPin = A2;
+// const int cPin = A1;
+// const int dPin = A0;
 
-// Input (control) pins:
-const int aPin = 4;
-const int bPin = 5;
-const int cPin = 6;
-const int dPin = 7;
-
-// Output pins
-const int y0 = 8; // Arduino pin 8, XR22-950 pin 12 (Y0)
-const int y1 = 9; // Arduino pin 9, XR22-950 pin 13 (Y1)
-const int y2 = 10; // Arduino pin 10, XR22-950 pin 14 (Y2)
-const int y3 = 11; // Arduino pin 16, XR22-950 pin 15 (Y3)
-const int y4 = 12; // Arduino pin 14, XR22-950 pin 16 (Y4)
+// Output pins are PB2 through PB6.
+// These aren't used because we need to use direct port manipulation to 
+// set the appropriate pin high for a very short duration, and digitalWrite
+// is too slow.
+// const int y0 = 16; // PB2, XR22-950 pin 12 (Y0)
+// const int y1 = 14; // PB3, XR22-950 pin 13 (Y1)
+// const int y2 = 8;  // PB4, XR22-950 pin 14 (Y2)
+// const int y3 = 9;  // PB5, XR22-950 pin 15 (Y3)
+// const int y4 = 10; // PB6, XR22-950 pin 16 (Y4)
 
 void setup() {
   Serial.begin(9600);
   Serial.println("Hello xr22-950");
 
   pinMode(outputEnabledPin, INPUT);
-  pinMode(aPin, INPUT);
-  pinMode(bPin, INPUT);
-  pinMode(cPin, INPUT);
-  pinMode(dPin, INPUT);
 
-  pinMode(y0, OUTPUT);
-  pinMode(y1, OUTPUT);
-  pinMode(y2, OUTPUT);
-  pinMode(y3, OUTPUT);
-  pinMode(y4, OUTPUT);
-
-  digitalWrite(y0, LOW);
-  digitalWrite(y1, LOW);
-  digitalWrite(y2, LOW);
-  digitalWrite(y3, LOW);
-  digitalWrite(y4, LOW);
+  // Input (control) pins are PF4 (D) through PF7 (A)
+  DDRF = B00000000;
+  // Output (splint) pins are PB2 through PB6
+  DDRB |= B01111100;
+  // Set outputs to low
+  PORTB = 0;
 
   setupSplintPins();
 
   attachInterrupt(digitalPinToInterrupt(outputEnabledPin), intHandler, FALLING);
 }
 
-int splintPins[16];
+byte splintPins[16];
 
 void setupSplintPins() {
   for (int i = 0; i < 16; ++i) {
     splintPins[i] = 0;
   }
-  // TODO: FIX THESE!
-  splintPins[0] = B00000001; // Arduino pin x, XR22-950 Pin 12 (Y0). This mostly works. Restores shift, ctrl, caps lock. Erratic behavior sometimes.
-  splintPins[1] = 0; // Arduino pin x, XR22-950 Pin 13 (Y1):
-  splintPins[2] = 0; // Arduino pin x, XR22-950 Pin 14 (Y2):
-  splintPins[3] = B00000100; // Arduino pin x, XR22-950 Pin 15 (Y3). This didn't work at all. Supposed to restore enter etc.
-  splintPins[3] = B00010000; // Arduino pin x, XR22-950 Pin 16 (Y4). This works, restores AZXCV, space
+
+  splintPins[0] = B00000100; // PB2, XR22-950 Pin 12 (Y0). Restores shift, ctrl, caps lock. Erratic behavior on prototype.
+  splintPins[1] = B00001000; // PB3, XR22-950 Pin 13 (Y1): Should restore the right half of the numeric keypad. Untested as of yet.
+  splintPins[2] = B00010000; // PB4, XR22-950 Pin 14 (Y2): Should restore the left half of the numeric keypad. Untested as of yet.
+  splintPins[3] = B00100000; // PB5, XR22-950 Pin 15 (Y3). Should restore right third of keys (enter, backspace), etc. This didn't work at all; possibly wiring issues.
+  splintPins[4] = B01000000; // PB6, XR22-950 Pin 16 (Y4). Restores AZXCV, space. Worked on prototype.
 }
 
 volatile int printed = 0; // debugging
 
 // Delay 1.4 microseconds. One nop = 62.5ns, so we use 22 nops (1.375 us)
-#define DELAY_14_US	"nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t"
+#define DELAY_14_US "nop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\tnop\n\t"
 
 void intHandler() {
   // Read pins a,b,c,d at the same time (upper 4 bits, then shift to the bottom nibble)
-  int controlPins = (PIND & B11110000) >> 4;
+  int controlPins = (PINF & B11110000) >> 4;
 
   if (printed == 100) {
     int a = (controlPins & B00000001),
@@ -85,13 +81,12 @@ void intHandler() {
     printed++;
   }
 
-  int outputPins = splintPin[controlPins];
-
+  byte outputPins = splintPin[controlPins];
   if (outputPins != 0) {
-    // TODO: FIXME
-    // PORTB = outputPins;
+    // Set all outputs on Port B
+    PORTB = outputPins;
     __asm__(DELAY_14_US);
-    // PORTB = B00000000;
+    PORTB = B00000000;
   }
 }
 
