1ef2fc562cf25a8ae7aadc52ec105ecb8ba9afba
David Plass <dplassgit@users.noreply.github.com>
2020-10-14 21:50:44 -0400

    Define the rest of the keys.

--- keyboardv1/keyboardv1.ino
+++ keyboardv1/keyboardv1.ino
@@ -1,43 +1,47 @@
+/*
+   Simple keyboard interface. Triggers when the data pin goes low.
+   Records 8 bits at 110 baud and looks up the value to send back.
+*/
+
 #include <Keyboard.h>
 
 #ifndef KEY_F12
-#warning "Using fake keyboard settings"
+
+// Arduino UNO can't be used as a HID device, but in order
+// to test the logic, we define some of the keys here.
+
+#warning "Using fake keyboard definitions"
 
 #define KEY_LEFT_CTRL   0x80
 #define KEY_LEFT_ALT    0x82
 
 #define KEY_UP_ARROW    0xDA
-#define KEY_DOWN_ARROW    0xD9
-#define KEY_LEFT_ARROW    0xD8
-#define KEY_RIGHT_ARROW   0xD7
+#define KEY_DOWN_ARROW  0xD9
+#define KEY_LEFT_ARROW  0xD8
+#define KEY_RIGHT_ARROW 0xD7
 #define KEY_BACKSPACE   0xB2
-#define KEY_TAB       0xB3
+#define KEY_TAB         0xB3
 #define KEY_RETURN      0xB0
-#define KEY_ESC       0xB1
+#define KEY_ESC         0xB1
 #define KEY_INSERT      0xD1
 #define KEY_DELETE      0xD4
 #define KEY_PAGE_UP     0xD3
 #define KEY_PAGE_DOWN   0xD6
-#define KEY_HOME      0xD2
-#define KEY_END       0xD5
-#define KEY_F1        0xC2
-#define KEY_F2        0xC3
-#define KEY_F3        0xC4
-#define KEY_F4        0xC5
-#define KEY_F5        0xC6
-#define KEY_F6        0xC7
-#define KEY_F7        0xC8
-#define KEY_F8        0xC9
-#define KEY_F9        0xCA
-#define KEY_F10       0xCB
-#define KEY_F11       0xCC
-#define KEY_F12       0xCD
+#define KEY_HOME        0xD2
+#define KEY_END         0xD5
+#define KEY_F1          0xC2
+#define KEY_F2          0xC3
+#define KEY_F3          0xC4
+#define KEY_F4          0xC5
+#define KEY_F5          0xC6
+#define KEY_F6          0xC7
+#define KEY_F7          0xC8
+#define KEY_F8          0xC9
+#define KEY_F9          0xCA
+#define KEY_F10         0xCB
+#define KEY_F11         0xCC
+#define KEY_F12         0xCD
 #endif
-
-/*
-   Simple keyboard interface. Triggers when the data pin goes low.
-   Records 8 bits at 110 baud and looks up the value to send back.
-*/
 
 const int dataPin = 7;      // pin 6 on the KB connector (and pin 6 of the RJ12)
 const int resetPin = 9;     // pin 2 on the KB connector (and pin 4 on the RJ12)
@@ -60,10 +64,47 @@ byte translationTable[256];
 byte numLockTable[256];
 
 #define SPECIAL 255
+
+#define VG_BACKSPACE 8
+#define VG_DEL 127
+// Maps to "Windows key"
+#define VG_LINEFEED 10
+
+#define VG_HELP 192
+#define VG_F1 193
+#define VG_F2 194
+#define VG_F3 195
+#define VG_F4 196
+#define VG_F5 197
+#define VG_F6 198
+#define VG_F7 199
+#define VG_F8 200
+#define VG_F9 201
+#define VG_F10 202
+#define VG_F11 203
+#define VG_F12 204
 #define VG_F13 205
 #define VG_F14 206
-#define VG_NP0 128
+
+// Numeric keypad:
 #define VG_NP_DOT 207
+#define VG_NP_0 128
+#define VG_NP_1 129
+#define VG_NP_2 130
+#define VG_NP_3 131
+#define VG_NP_4 132
+#define VG_NP_5 133
+#define VG_NP_6 134
+#define VG_NP_7 135
+#define VG_NP_8 136
+#define VG_NP_9 167
+#define VG_NP_DASH 142
+#define VG_NP_COMMA 143
+#define VG_NP_UP 138
+#define VG_NP_DOWN 139
+#define VG_NP_LEFT 140
+#define VG_NP_RIGHT 141
+#define VG_NP_ENTER 160
 
 void setupTable() {
   for (int i = 0; i < 256; i++)  {
@@ -80,66 +121,67 @@ void setupTable() {
     numLockTable[i] = translationTable[i];
   }
 
-  //translationTable[VG_BACKSPACE] = ;
-  translationTable[8] = KEY_TAB;
+  translationTable[9] = KEY_TAB;
+  // maybe this should be alt, and F13 is Windows?
+  translationTable[VG_LINEFEED] = KEY_LEFT_GUI; // Windows key, by itself. Will not work as-is, as a modifier
   translationTable[13] = KEY_RETURN;
   translationTable[27] = KEY_ESC;
+  translationTable[VG_DEL] = KEY_DELETE;
+  translationTable[VG_BACKSPACE] = KEY_BACKSPACE;
 
-  translationTable[192] = KEY_F1; // map vg help key to f1
-  translationTable[193] = KEY_F2; // map vg f1 to f2
-  translationTable[194] = KEY_F3; // map vg f2 to f3
-  translationTable[195] = KEY_F4; // map vg f3 to f4
-  translationTable[196] = KEY_F5; // map vg f4 to f6
-  translationTable[197] = KEY_F6; // map vg f5 to f6
+  translationTable[VG_HELP] = KEY_F1; // map vg help key to f1
+  translationTable[VG_F1] = KEY_F2; // map vg f1 to f2
+  translationTable[VG_F2] = KEY_F3; // map vg f2 to f3
+  translationTable[VG_F3] = KEY_F4; // map vg f3 to f4
+  translationTable[VG_F4] = KEY_F5; // map vg f4 to f6
+  translationTable[VG_F5] = KEY_F6; // map vg f5 to f6
   // NOTE: NO 198 (F6), because you can't type F6 on this keyboard...
-  translationTable[199] = KEY_F7; // FROM HERE, the mappings are correct
-  translationTable[200] = KEY_F8;
-  translationTable[201] = KEY_F9;
-  translationTable[202] = KEY_F10;
-  translationTable[203] = KEY_F11;
-  translationTable[204] = KEY_F12;
+  translationTable[VG_F7] = KEY_F7; // FROM HERE, the mappings are correct
+  translationTable[VG_F8] = KEY_F8;
+  translationTable[VG_F9] = KEY_F9;
+  translationTable[VG_F10] = KEY_F10;
+  translationTable[VG_F11] = KEY_F11;
+  translationTable[VG_F12] = KEY_F12;
   translationTable[VG_F13] = SPECIAL;
   translationTable[VG_F14] = SPECIAL;
 
-  // translationTable[VG_UP] = KEY_UP_ARROW;
-  // translationTable[VG_DOWN] = KEY_DOWN_ARROW;
-  // translationTable[VG_LEFT] = KEY_LEFT_ARROW;
-  // translationTable[VG_RIGHT] = KEY_RIGHT_ARROW;
-  // translationTable[VG_NP_ENTER] = KEY_RETURN;
+  translationTable[VG_UP] = KEY_UP_ARROW;
+  translationTable[VG_DOWN] = KEY_DOWN_ARROW;
+  translationTable[VG_LEFT] = KEY_LEFT_ARROW;
+  translationTable[VG_RIGHT] = KEY_RIGHT_ARROW;
+  translationTable[VG_NP_ENTER] = KEY_RETURN;
 
   // Numlock off: cursor keys:
-  translationTable[VG_NP0] = KEY_INSERT;
-  // translationTable{VG_NP_1] = KEY_END;
-  // translationTable{VG_NP_2] = KEY_DOWN_ARROW;
-  // translationTable{VG_NP_3] = KEY_PAGE_DOWN;
-  // translationTable{VG_NP_4] = KEY_LEFT_ARROW;
-  // translationTable{VG_NP_3] = '5';
-  // translationTable{VG_NP_6] = KEY_RIGHT_ARROW;
-  // translationTable{VG_NP_7] = KEY_HOME;
-  // translationTable{VG_NP_8] = KEY_UP_ARROW;
-  // translationTable{VG_NP_9] = KEY_PAGE_UP;
-  // translationTable[VG_NP_COMMA] = ',';
+  translationTable[VG_NP_0] = KEY_INSERT;
+  translationTable[VG_NP_1] = KEY_END;
+  translationTable[VG_NP_2] = KEY_DOWN_ARROW;
+  translationTable[VG_NP_3] = KEY_PAGE_DOWN;
+  translationTable[VG_NP_4] = KEY_LEFT_ARROW;
+  translationTable[VG_NP_5] = '5';
+  translationTable[VG_NP_6] = KEY_RIGHT_ARROW;
+  translationTable[VG_NP_7] = KEY_HOME;
+  translationTable[VG_NP_8] = KEY_UP_ARROW;
+  translationTable[VG_NP_9] = KEY_PAGE_UP;
+  translationTable[VG_NP_COMMA] = ',';
+  translationTable[VG_NP_DASH] = '-';
   translationTable[VG_NP_DOT] = KEY_DELETE;
 
-  // translationTable[VG_NP_MINUS] = '-';
   for (int i = 0; i < 256; i++)  {
     numLockTable[i] = translationTable[i];
   }
 
   // Numlock overrides:
-  numLockTable[VG_NP0] = '0';
-  // numLockTable[VG_NP_1] = '1';
-  // numLockTable[VG_NP_2] = '2';
-  // numLockTable[VG_NP_3] = '3';
-  // numLockTable[VG_NP_4] = '4';
-  // numLockTable[VG_NP_5] = '5';
-  // numLockTable[VG_NP_6] = '6';
-  // numLockTable[VG_NP_7] = '7';
-  // numLockTable[VG_NP_8] = '8';
-  // numLockTable[VG_NP_9] = '9';
-  // numLockTable[VG_NP_COMMA] = ',';
+  numLockTable[VG_NP_0] = '0';
+  numLockTable[VG_NP_1] = '1';
+  numLockTable[VG_NP_2] = '2';
+  numLockTable[VG_NP_3] = '3';
+  numLockTable[VG_NP_4] = '4';
+  numLockTable[VG_NP_5] = '5';
+  numLockTable[VG_NP_6] = '6';
+  numLockTable[VG_NP_7] = '7';
+  numLockTable[VG_NP_8] = '8';
+  numLockTable[VG_NP_9] = '9';
   numLockTable[VG_NP_DOT] = '.';
-  // numlockTable[VG_NP_MINUS] = '-';
 }
 
 
@@ -214,6 +256,7 @@ void sendChar(byte key) {
   } else {
     translated = translationTable[key];
   }
+
   if (translated == SPECIAL) {
     // deal with F13 & F14
     if (key == VG_F13) {
