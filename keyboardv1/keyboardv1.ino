58406af7a7176bf738b7dfa9aa5d37e6ce0402b0
David Plass <dplassgit@users.noreply.github.com>
2020-10-25 15:42:14 -0400

    Change the enable/disable key to ctrl+shift+f6; moved translation logic

--- keyboardv1/keyboardv1.ino
+++ keyboardv1/keyboardv1.ino
@@ -2,45 +2,8 @@
    Simple keyboard interface. Triggers when the data pin goes low.
    Records 8 bits at 110 baud and looks up the value to send back.
 */
-
 #include <Keyboard.h>
-
-#ifndef KEY_F12
-// Arduino UNO can't be used as a HID device, but in order
-// to test the logic, we define some of the keys here.
-
-#warning "Using fake keyboard definitions"
-
-#define KEY_LEFT_CTRL   0x80
-#define KEY_LEFT_ALT    0x82
-
-#define KEY_UP_ARROW    0xDA
-#define KEY_DOWN_ARROW  0xD9
-#define KEY_LEFT_ARROW  0xD8
-#define KEY_RIGHT_ARROW 0xD7
-#define KEY_BACKSPACE   0xB2
-#define KEY_TAB         0xB3
-#define KEY_RETURN      0xB0
-#define KEY_ESC         0xB1
-#define KEY_INSERT      0xD1
-#define KEY_DELETE      0xD4
-#define KEY_PAGE_UP     0xD3
-#define KEY_PAGE_DOWN   0xD6
-#define KEY_HOME        0xD2
-#define KEY_END         0xD5
-#define KEY_F1          0xC2
-#define KEY_F2          0xC3
-#define KEY_F3          0xC4
-#define KEY_F4          0xC5
-#define KEY_F5          0xC6
-#define KEY_F6          0xC7
-#define KEY_F7          0xC8
-#define KEY_F8          0xC9
-#define KEY_F9          0xCA
-#define KEY_F10         0xCB
-#define KEY_F11         0xCC
-#define KEY_F12         0xCD
-#endif
+#include "vg_keys.h"
 
 bool useSerialLibrary = true;
 
@@ -75,46 +38,6 @@ byte numLockTable[256];
 
 #define SPECIAL 255
 
-#define VG_BACKSPACE 8
-#define VG_DEL 127
-// Maps to "Windows key"
-#define VG_LINEFEED 10
-
-#define VG_HELP 192
-#define VG_F1 193
-#define VG_F2 194
-#define VG_F3 195
-#define VG_F4 196
-#define VG_F5 197
-#define VG_F6 198
-#define VG_F7 199
-#define VG_F8 200
-#define VG_F9 201
-#define VG_F10 202
-#define VG_F11 203
-#define VG_F12 204
-#define VG_F13 205
-#define VG_F14 206
-
-// Numeric keypad:
-#define VG_NP_DOT 207
-#define VG_NP_0 128
-#define VG_NP_1 129
-#define VG_NP_2 130
-#define VG_NP_3 131
-#define VG_NP_4 132
-#define VG_NP_5 133
-#define VG_NP_6 134
-#define VG_NP_7 135
-#define VG_NP_8 136
-#define VG_NP_9 137
-#define VG_NP_DASH 142
-#define VG_NP_COMMA 143
-#define VG_UP 138
-#define VG_DOWN 139
-#define VG_LEFT 140
-#define VG_RIGHT 141
-#define VG_NP_ENTER 160
 
 void setupTable() {
   for (int i = 0; i < 256; i++)  {
@@ -131,14 +54,16 @@ void setupTable() {
     numLockTable[i] = translationTable[i];
   }
 
-  translationTable[9] = KEY_TAB;
+  translationTable[VG_TAB] = KEY_TAB;
   // maybe this should be alt, and F13 is Windows?
   translationTable[VG_LINEFEED] = KEY_LEFT_GUI; // Windows key, by itself. Will not work as-is, as a modifier
   translationTable[13] = KEY_RETURN;
-  translationTable[27] = KEY_ESC;
-  translationTable[29] = SPECIAL; // shift-tab
-  translationTable[VG_DEL] = KEY_DELETE;
-  translationTable[VG_BACKSPACE] = KEY_BACKSPACE;
+  translationTable[VG_ESC] = KEY_ESC;
+  // translationTable[29] = SPECIAL; // shift-tab
+  // translationTable[28] = SPECIAL; // control-backslash
+  // Yes, delete and backspace are swapped.
+  translationTable[VG_DEL] = KEY_BACKSPACE;
+  translationTable[VG_BACKSPACE] = KEY_DELETE;
 
   translationTable[VG_F1] = KEY_F1;
   translationTable[VG_F2] = KEY_F2;
@@ -151,7 +76,7 @@ void setupTable() {
   translationTable[VG_F9] = KEY_F9;
   translationTable[VG_F10] = KEY_F10;
   translationTable[VG_F11] = KEY_F11;
-  translationTable[VG_F12] = KEY_F12;
+  translationTable[VG_F12] = SPECIAL; // KEY_F12;
   translationTable[VG_F13] = SPECIAL;
   translationTable[VG_F14] = SPECIAL;
 
@@ -159,6 +84,7 @@ void setupTable() {
   translationTable[VG_DOWN] = KEY_DOWN_ARROW;
   translationTable[VG_LEFT] = KEY_LEFT_ARROW;
   translationTable[VG_RIGHT] = KEY_RIGHT_ARROW;
+
   translationTable[VG_NP_ENTER] = KEY_RETURN;
 
   // Numlock off: cursor keys:
@@ -242,8 +168,16 @@ bool nextIsAlt = false;
 bool numLock = false;
 
 void sendChar(byte key) {
-  if (key == VG_F6) {
+  if (key == 246) { // ctrl+shift+F6 (help)
+    // Toggles the keyboard-iness
     setUseSerialLibrary(!useSerialLibrary);
+  }
+
+  byte translated;
+  if (numLock) {
+    translated = numLockTable[key];
+  } else {
+    translated = translationTable[key];
   }
 
   // Figure out what to do with the key
@@ -252,22 +186,23 @@ void sendChar(byte key) {
   //  * F13 is "send next char as "alt""
   //  * F14 is "numLock toggle"
   //  * Other characters are translated.
-  if (key >= ' ' && key <= '~') {
-    if (nextIsAlt) {
-      if (useSerialLibrary) {
-        Serial.print("Alt+"); Serial.println((char) key);
-      } else {
-        Keyboard.begin();
-        Keyboard.press(KEY_LEFT_ALT);
-        Keyboard.press(key);
-        delay(20); // I've seen this elsewhere. sometimes 100 ms
-        Keyboard.releaseAll();
-        Keyboard.end();
-      }
-      nextIsAlt = false;
-      return;
+  if (nextIsAlt) {
+    if (useSerialLibrary) {
+      Serial.print("Alt+"); Serial.println(translated);
+    } else {
+      // TODO: think about alt+ctrl+char, and alt+ctrl+shift+char
+      Keyboard.begin();
+      Keyboard.press(KEY_LEFT_ALT);
+      Keyboard.write(translated);
+      delay(100); // I've seen this elsewhere. sometimes 100 ms
+      Keyboard.releaseAll();
+      Keyboard.end();
     }
+    nextIsAlt = false;
+    return;
+  }
 
+  if (key >= ' ' && key <= '~') {
     if (useSerialLibrary) {
       Serial.print("Printable: "); Serial.println((char) key);
     } else {
@@ -278,19 +213,11 @@ void sendChar(byte key) {
     return;
   }
 
-  // TODO: think about alt+ctrl+char, and alt+ctrl+shift+char
-  byte translated;
-  if (numLock) {
-    translated = numLockTable[key];
-  } else {
-    translated = translationTable[key];
-  }
-
   if (translated == SPECIAL) {
     // deal with F13 & F14
-    if (key == VG_F13) {
+    if (key == VG_F12) {
       if (useSerialLibrary) {
-        Serial.println("F13: Next-alt");
+        Serial.println("F12: Next-alt");
       }
       nextIsAlt = true;
     } else if (key == VG_F14) {
