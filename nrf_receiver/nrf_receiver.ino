3641b791dffcd8e059b81598acebe5e02b850406
David Plass <dplassgit@users.noreply.github.com>
2021-04-06 11:59:32 -0400

    Removed multiple thermometers; added a "whathappened" call

--- nrf_receiver/nrf_receiver.ino
+++ nrf_receiver/nrf_receiver.ino
@@ -1,27 +1,18 @@
 #include <SoftwareSerial.h>
 #include <RF24.h>
-
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
 #include "data.h"
 
 #define ONE_WIRE_BUS 2
-
-// Set up a oneWire instance to communicate with any OneWire device
 OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass oneWire reference to DallasTemperature library
 DallasTemperature sensors(&oneWire);
-// Number of thermometers
-int deviceCount;
 
 #define BAUD_RATE 57600
-
 SoftwareSerial mySerial(4, 5); // RX, TX
 
-RF24 radio(A0, 8);  // CE (disconnected), CSN
-
+RF24 radio(9, 8);  // CE (disconnected), CSN
 #define NRF_INT_PIN 3
 
 // Address through which two modules communicate over the radio.
@@ -48,35 +39,35 @@ void setup() {
   radio.startListening();
 
   sensors.begin();  // Start up the DS18B library
-  Serial.print("Locating DS18Bs...");
-  Serial.print("Found ");
-  deviceCount = sensors.getDeviceCount();
-  Serial.print(deviceCount);
-  Serial.println(" devices.");
+
   attachInterrupt(digitalPinToInterrupt(NRF_INT_PIN), intHandler, FALLING);
 }
 
 void intHandler() {
-  if (radio.available()) {
-    struct Data remoteData;
-    radio.read(&remoteData, DATA_SIZE);
-    char temp[32];
-    snprintf_P(temp,
-               sizeof(temp),
-               PSTR("%c%c: %d F %d V"),
-               remoteData.counter,
-               remoteData.id,
-               (short)remoteData.tempF,
-               remoteData.voltage);
-    Serial.print("Data received equivalent to: ");
-    Serial.println(temp);
-    int sent = mySerial.write((byte*) &remoteData,DATA_SIZE);
-    if (sent != DATA_SIZE) {
-      Serial.println("Forwarding FAILED");
+  bool txok, txfail, rxread;
+  radio.whatHappened(txok, txfail, rxread);
+  if (rxread) {
+    if (radio.available()) {
+      struct Data remoteData;
+      radio.read(&remoteData, DATA_SIZE);
+      char temp[32];
+      snprintf_P(temp,
+                 sizeof(temp),
+                 PSTR("%c%c: %d F %d V"),
+                 remoteData.counter,
+                 remoteData.id,
+                 (short)remoteData.tempF,
+                 remoteData.voltage);
+      Serial.print("Data received ~: ");
+      Serial.println(temp);
+      int sent = mySerial.write((byte*) &remoteData, DATA_SIZE);
+      if (sent != DATA_SIZE) {
+        Serial.println("Forwarding FAILED");
+      }
+      return;
     }
-  } else {
-    Serial.println("Not available");
   }
+  Serial.println("Not available");
 }
 
 int counter = 0;
@@ -85,24 +76,16 @@ void loop() {
   // Send command to all the sensors for temperature conversion
   sensors.requestTemperatures();
 
-  float sendingTemp;
-  // Display temperature from each sensor
-  for (int i = 0; i < deviceCount; ++i) {
-    Serial.print(i);
-    Serial.print(" Temp: ");
-    float tempC = sensors.getTempCByIndex(i);
-    Serial.print(tempC);
-    Serial.print("C  |  ");
-    float tempF = DallasTemperature::toFahrenheit(tempC);
-    if (i == 0) {
-      sendingTemp = tempF;
-    }
-    Serial.print(tempF);
-    Serial.println("F");
-  }
+  Serial.print("Local temperature: ");
+  float tempC = sensors.getTempCByIndex(0);
+  Serial.print(tempC);
+  Serial.print("C | ");
+  float tempF = DallasTemperature::toFahrenheit(tempC);
+  Serial.print(tempF);
+  Serial.println("F");
 
   // Send message to base
-  data.tempF = sendingTemp;
+  data.tempF = tempF;
   data.counter = 'A' + counter;
   char text[32];
   snprintf_P(text,
@@ -113,7 +96,7 @@ void loop() {
              (short)data.tempF,
              data.voltage
             );
-  Serial.print("Trying to send equivalent of: "); Serial.println(text);
+  Serial.print("Trying to send ~: "); Serial.println(text);
   int sent = mySerial.write((byte*) &data, DATA_SIZE);
   if (sent != DATA_SIZE) {
     Serial.println("Sending failed");
