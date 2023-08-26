736169e34d1aee2a5970bb3caa5ee290e4d53fbc
David Plass <dplassgit@users.noreply.github.com>
2023-08-26 16:09:20 -0400

    Adjusted seven segment word clock to be more wordy.

--- sevensegwordclock/sevensegwordclock.ino
+++ sevensegwordclock/sevensegwordclock.ino
@@ -119,7 +119,7 @@ void populateTimeStringAsWords(struct tm * dt) {
   const char *minuteStr = getMinute(minute);
   char temp[BUFFER_SIZE];
 
-  if (minute >= 58 || minute <= 3) {
+  if (minute >= 58 || minute <= 2 || (minute >= 33 && minute <= 42)) {
     if (hour == 0 || hour == 12 || hour == 24) {
       // don't show "noon o'clock" or "midnight o'clock"
       minuteStr = "";
@@ -178,13 +178,13 @@ const char *getMinute(int minute) {
     case 58: case 59: case 0:  case 1:  case 2: return "O'clocK";
     case 3:  case 4:  case 5:  case 6:  case 7: return "Five Past";
     case 8:  case 9:  case 10: case 11: case 12: return "Ten Past";
-    case 13: case 14: case 15: case 16: case 17: return "Quarter Past";
+    case 13: case 14: case 15: case 16: case 17: return "a Quarter Past";
     case 18: case 19: case 20: case 21: case 22: return "20 Past";
     case 23: case 24: case 25: case 26: case 27: return "25 Past";
     case 28: case 29: case 30: case 31: case 32: return "Half Past";
-    case 33: case 34: case 35: case 36: case 37: return "25 to";
-    case 38: case 39: case 40: case 41: case 42: return "20 to";
-    case 43: case 44: case 45: case 46: case 47: return "Quarter to";
+    case 33: case 34: case 35: case 36: case 37: return "Thirty five";
+    case 38: case 39: case 40: case 41: case 42: return "Forty";
+    case 43: case 44: case 45: case 46: case 47: return "a Quarter to";
     case 48: case 49: case 50: case 51: case 52: return "Ten to";
     case 53: case 54: case 55: case 56: case 57: return "Five to";
     default: return "";
